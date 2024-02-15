119bcf8e6ec758c8cef0d294e2c1228d1b2c56f3
Abdou00 <abdoulaye_diarra@outlook.fr>
2024-02-15 09:24:23 +0100

    setup plugin

---
+++ src/TemperatureConverter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Samba\MyDigitalSchoolPlugin;
+class TemperatureConverter
+{
+    public function convert($temperature, $unite) {
+        if ($unite === 'C') {
+            $fahrenheit = ($temperature * 9 / 5) + 32;
+            return $fahrenheit;
+        } else if ($unite === 'F') {
+            $celsuis = ($temperature - 32) * 5 / 9;
+            return $celsuis;
+        } else {
+            throw new Exception("Error Processing Request", 1);
+        }
+        
+    }
+}
